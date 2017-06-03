25c5efb376d0c0aa8e21f236c2b7b8da9e30b958
Kazuho Niidome <acque.2two@gmail.com>
2017-06-03 21:26:08 +0900

    draw implemented

--- app/src/main/java/tech/wcdi/spajam/myapplication/Graphics/Rect/Square.java
+++ app/src/main/java/tech/wcdi/spajam/myapplication/Graphics/Rect/Square.java
@@ -1,43 +1,50 @@
 package tech.wcdi.spajam.myapplication.Graphics.Rect;
 
+/**
+ * Created by G015C1143 on 2017/06/03.
+ */
+
 import android.opengl.GLES20;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 import tech.wcdi.spajam.myapplication.Graphics.OGLRenderer;
 
 /**
- * A two-dimensional triangle for use as a drawn object in OpenGL ES 2.0.
+ * A two-dimensional square for use as a drawn object in OpenGL ES 2.0.
  */
-public class Triangle {
+public class Square {
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
-    static float triangleCoords[] = {
-            // in counterclockwise order:
-            0.0f, 0.622008459f, 0.0f,   // top
-            -0.5f, -0.311004243f, 0.0f,   // bottom left
-            0.5f, -0.311004243f, 0.0f    // bottom right
-    };
+    static float squareCoords[] = {-0.05f, 0.0f, 0.0f,   // top left
+                                   0.0f, -0.05f, 0.0f,   // bottom left
+                                   0.05f, 0.0f, 0.0f,   // bottom right
+                                   0.0f, 0.05f, 0.0f}; // top right
     private final String vertexShaderCode =
             // This matrix member variable provides a hook to manipulate
             // the coordinates of the objects that use this vertex shader
             "uniform mat4 uMVPMatrix;" + "attribute vec4 vPosition;" + "void main() {" +
-                    // the matrix must be included as a modifier of gl_Position
+                    // The matrix must be included as a modifier of gl_Position.
                     // Note that the uMVPMatrix factor *must be first* in order
                     // for the matrix multiplication product to be correct.
                     "  gl_Position = uMVPMatrix * vPosition;" + "}";
-    private final String fragmentShaderCode =
-            "precision mediump float;" + "uniform vec4 vColor;" +
-                    "void main() {" + "  gl_FragColor = vColor;" +
+    private final String
+            fragmentShaderCode =
+            "precision mediump float;" +
+                    "uniform vec4 vColor;" +
+                    "void main() {" +
+                    "  gl_FragColor = vColor;" +
                     "}";
     private final FloatBuffer vertexBuffer;
+    private final ShortBuffer drawListBuffer;
     private final int mProgram;
-    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+    private final short drawOrder[] = {0, 1, 2, 0, 2, 3}; // order to draw vertices
     private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
-    float color[] = {0.63671875f, 0.76953125f, 0.22265625f, 0.0f};
+    float color[] = {0.2f, 0.709803922f, 0.898039216f, 1.0f};
     private int mPositionHandle;
     private int mColorHandle;
     private int mMVPMatrixHandle;
@@ -45,20 +52,24 @@ public class Triangle {
     /**
      * Sets up the drawing object data for use in an OpenGL ES context.
      */
-    public Triangle() {
+    public Square() {
         // initialize vertex byte buffer for shape coordinates
         ByteBuffer bb = ByteBuffer.allocateDirect(
-                // (number of coordinate values * 4 bytes per float)
-                triangleCoords.length * 4);
-        // use the device hardware's native byte order
+                // (# of coordinate values * 4 bytes per float)
+                squareCoords.length * 4);
         bb.order(ByteOrder.nativeOrder());
-
-        // create a floating point buffer from the ByteBuffer
         vertexBuffer = bb.asFloatBuffer();
-        // add the coordinates to the FloatBuffer
-        vertexBuffer.put(triangleCoords);
-        // set the buffer to read the first coordinate
+        vertexBuffer.put(squareCoords);
         vertexBuffer.position(0);
+
+        // initialize byte buffer for the draw list
+        ByteBuffer dlb = ByteBuffer.allocateDirect(
+                // (# of coordinate values * 2 bytes per short)
+                drawOrder.length * 2);
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
+        drawListBuffer.put(drawOrder);
+        drawListBuffer.position(0);
 
         // prepare shaders and OpenGL program
         int vertexShader = OGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
@@ -68,7 +79,6 @@ public class Triangle {
         GLES20.glAttachShader(mProgram, vertexShader);   // add the vertex shader to program
         GLES20.glAttachShader(mProgram, fragmentShader); // add the fragment shader to program
         GLES20.glLinkProgram(mProgram);                  // create OpenGL program executables
-
     }
 
     /**
@@ -109,11 +119,13 @@ public class Triangle {
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
         OGLRenderer.checkGlError("glUniformMatrix4fv");
 
-        // Draw the triangle
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+        // Draw the square
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES,
+                drawOrder.length,
+                GLES20.GL_UNSIGNED_SHORT,
+                drawListBuffer);
 
         // Disable vertex array
         GLES20.glDisableVertexAttribArray(mPositionHandle);
     }
-
 }
