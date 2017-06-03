ffe07e338efb53b9ebe0fabbbf81f7a4c4499102
Yuya Otsuka <otsuka.main@google.com>
2017-06-04 08:06:40 +0900

    add: mjk

--- app/src/main/assets/js/main.js
+++ app/src/main/assets/js/main.js
@@ -1,2 +1,28 @@
-document.body.innerHTML = "test"
+console.log("test")
 
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(0, 6, 0)
+
+var renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+var light = new THREE.PointLight(0xffffff)
+light.position.set(0, 0, 0)
+
+var scene = new THREE.Scene()
+scene.add(camera)
+scene.add(light)
+
+document.body.appendChild(renderer.domElement)
+
+var stars = [
+    [0, 0, 0]
+]
+
+var render = function () {
+    requestAnimationFrame(render)
+
+    renderer.render(scene, camera)
+}
+
+render()
