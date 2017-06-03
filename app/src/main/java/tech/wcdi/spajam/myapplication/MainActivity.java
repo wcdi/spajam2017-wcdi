443f1a0736f1a0cacd0dbfd3c0cb41a59e668969
Yuya Otsuka <otsuka.main@google.com>
2017-06-03 15:36:23 +0900

    add: メイン画面の表示を実装

--- app/src/main/java/tech/wcdi/spajam/myapplication/MainActivity.java
+++ app/src/main/java/tech/wcdi/spajam/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package tech.wcdi.spajam.myapplication;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getFragmentManager();
+
+        manager.beginTransaction()
+            .add(R.id.content_frame, MainFragment.newInstance())
+            .commit();
     }
 }
