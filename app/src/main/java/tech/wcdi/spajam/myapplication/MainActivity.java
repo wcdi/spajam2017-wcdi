be2b50868c3289264fec0f719bb35e3281e7a24f
Naoki Kanazawa <kanazawa@naoki912.net>
2017-06-04 04:28:18 +0900

    [WIP] 5000兆円欲しい！

--- app/src/main/java/tech/wcdi/spajam/myapplication/MainActivity.java
+++ app/src/main/java/tech/wcdi/spajam/myapplication/MainActivity.java
@@ -3,8 +3,14 @@ package tech.wcdi.spajam.myapplication;
 import android.app.FragmentManager;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,18 +21,22 @@ import com.firebase.ui.auth.IdpResponse;
 
 import java.io.File;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     public static SensorManager mSensorManager;
+    public static DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
+
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         final FragmentManager manager = getFragmentManager();
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
         manager.beginTransaction()
             .add(
                 R.id.content_frame,
@@ -73,14 +83,43 @@ public class MainActivity extends AppCompatActivity {
 
         ListView drawerList = (ListView) findViewById(R.id.left_drawer);
 
-        drawerList.setAdapter(
-            new ArrayAdapter<String>(this, R.layout.drawer_list_item)
-        );
+//        ListView drawerList = (ListView) findViewById(R.id.left_drawer);
 
-        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            }
-        });
+//        drawerList.setAdapter(
+//            new ArrayAdapter<String>(this, R.layout.drawer_list_item)
+//        );
+
+//        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            }
+//        });
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
