33dd92ff3c118fcd0fdfff2c049d4d4a8b0abe08
Kazuho Niidome <acque.2two@gmail.com>
2017-06-04 07:15:18 +0900

    Downloading view fixed

--- app/src/main/java/tech/wcdi/spajam/myapplication/transfer/AsyncSender.java
+++ app/src/main/java/tech/wcdi/spajam/myapplication/transfer/AsyncSender.java
@@ -18,60 +18,60 @@ import okhttp3.Response;
 
 public class AsyncSender {
 
-        public final MediaType jsontype = MediaType.parse("application/json; charset=utf-8");
-        public String urlPath = "/index.html";
-        // 入力データ群
-        // 全データ
-        public Data allData;
-        public OkHttpClient client = new OkHttpClient();
-        public String url;
-        // 受信したデータ
-        Data reply;
+    public final MediaType jsontype = MediaType.parse("application/json; charset=utf-8");
+    public String urlPath = "/index.html";
+    // 入力データ群
+    // 全データ
+    public Data allData;
+    public OkHttpClient client = new OkHttpClient();
+    public String url;
+    // 受信したデータ
+    Data reply;
     private String URL = "http://server.wcdi.tech/";
     private final String urlHosts = URL;
 
-        public void setPath(String path) {
-            this.url = this.urlHosts + path;
+    public void setPath(String path) {
+        this.url = this.urlHosts + path;
+    }
+
+    public Data getReply() {
+        return reply;
+    }
+
+    public boolean isSuccess() {
+        return reply.isStatus();
+    }
+
+
+    private String Post(String json) throws IOException {
+        Log.d("TRANS/POST/START", "Post function start.");
+        RequestBody body = RequestBody.create(jsontype, json);
+        Request request = new Request.Builder().url(url).addHeader("Content-Language", "ja")
+
+                .post(body).build();
+        Response response = client.newCall(request).execute();
+        if (response.code() != 200) {
+            Log.e("TRANS/POST/ERROR", "Responce code is " + response.code() + "!");
+            return "{\"status\":false}";
+        }
+        Log.d("TRANS/POST/SUCCESS",
+                "POST Successful! " + response.code() + "! " + response.body().contentLength());
+        return response.body().string();
+    }
+
+    protected Data doInBackground(Data... params) {
+        try {
+            Log.d("TRANSFER/START", "Download start...");
+            Log.d("TRANSFER/START/URL", url);
+            Log.d("TRANSFER/START/DATA", JSON.encode(allData));
+
+            reply = JSON.decode(Post("q=" + JSON.encode(allData)), Data.class);
+        } catch (IOException e) {
+            Log.e("TRANSFER/ERROR/EXCEPT", "IOException Error");
+            e.printStackTrace();
         }
 
-        public Data getReply() {
-            return reply;
-        }
-
-        public boolean isSuccess() {
-            return reply.isStatus();
-        }
-
-
-        private String Post(String json) throws IOException {
-            Log.d("TRANS/POST/START", "Post function start.");
-            RequestBody body = RequestBody.create(jsontype, json);
-            Request request = new Request.Builder().url(url).addHeader("Content-Language", "ja")
-
-                    .post(body).build();
-            Response response = client.newCall(request).execute();
-            if (response.code() != 200) {
-                Log.e("TRANS/POST/ERROR", "Responce code is " + response.code() + "!");
-                return "{\"status\":false}";
-            }
-            Log.d("TRANS/POST/SUCCESS",
-                    "POST Successful! " + response.code() + "! " + response.body().contentLength());
-            return response.body().string();
-        }
-
-        protected Data doInBackground(Data... params) {
-            try {
-                Log.d("TRANSFER/START", "Download start...");
-                Log.d("TRANSFER/START/URL", url);
-                Log.d("TRANSFER/START/DATA", JSON.encode(allData));
-
-                reply = JSON.decode(Post("q=" + JSON.encode(allData)), Data.class);
-            } catch (IOException e) {
-                Log.e("TRANSFER/ERROR/EXCEPT", "IOException Error");
-                e.printStackTrace();
-            }
-
-            Log.d("TRANSFER/FINISH", "TRANSFER FINISHED.");
-            return null;
-        }
+        Log.d("TRANSFER/FINISH", "TRANSFER FINISHED.");
+        return null;
+    }
 }
